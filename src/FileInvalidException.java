35eac48a54501b62a33e447648fc6e65edad3ae9
jose salazar <joshrs23@gmail.com>
2023-03-31 15:21:40 -0400

    exceptions to the fields

--- src/FileInvalidException.java
+++ src/FileInvalidException.java
@@ -1,4 +1,11 @@
 
-public class FileInvalidException {
-
+public class FileInvalidException extends Exception{
+	
+	public FileInvalidException() {
+		super("Error: Input file cannot be parsed due to missing information (i.e. month={}, title={}, etc.)");
+	}
+	
+	public FileInvalidException(String file,String field) {
+		super("Error: Detected Empty Field!\n==============================\n\nProblem detected with input file:"+file+"\nFile is invalid: Field "+field+" is Empty. Processing stopped at this point. Other empty fields may be present as well");
+	}
 }
